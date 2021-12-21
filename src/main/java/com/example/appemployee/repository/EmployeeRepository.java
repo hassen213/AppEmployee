cb01de8aaf53ef2314b2369f706cd0b313ac9872
zeighoum <hassen.zeighm@gmail.com>
2021-12-21 10:48:59 +0100

    create the data base and the controller

---
+++ src/main/java/com/example/appemployee/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.appemployee.repository;
+
+import com.example.appemployee.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
