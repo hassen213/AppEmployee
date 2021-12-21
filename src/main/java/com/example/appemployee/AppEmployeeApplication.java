31983c89ae0ff9db9403dc8d6d9019e7c10e6005
zeighoum <hassen.zeighm@gmail.com>
2021-12-21 10:37:00 +0100

    first commit

---
+++ src/main/java/com/example/appemployee/AppEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.appemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppEmployeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/appemployee/AppEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEmployeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
